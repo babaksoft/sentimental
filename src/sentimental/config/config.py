21efb89765e6efc72ab7eac2a11143fd4555ee68
babaksoft <babak.eslamieh@gmail.com>
2026-03-03 22:40:41 +0330

    Add Data Ingestion experiment (train set not OK) --> Update DVC

--- src/sentimental/config/config.py
+++ src/sentimental/config/config.py
@@ -2,24 +2,26 @@ from pathlib import Path
 
 from sklearn.linear_model import LogisticRegression
 
-
+# Global config
 RANDOM_STATE = 147
-
 PROJECT_NAME = "sentimental"
+MLFLOW_TRACKING_URI = "http://localhost:5000/"
 PACKAGE_ROOT = Path(__file__).resolve().parent.parent
 
+# Path config
 DATA_PATH = PACKAGE_ROOT / "data"
 MODEL_PATH = PACKAGE_ROOT / "model"
 METRICS_PATH = PACKAGE_ROOT / "metrics"
 
-RAW_FILE = "" # Name of raw CSV dataset
+# Data ingestion config
+TARGET = "status" # Needs stratification
+RAW_FILE = "mental_health.csv"
 TRAIN_FILE = "train.csv"
 VALIDATION_FILE = "validation.csv"
 TEST_FILE = "test.csv"
-TRAIN_SPLIT = 0.7
-VAL_SPLIT = 0.15
-TEST_SPLIT = 0.15
+TRAIN_TEST_SPLIT = 0.3 # Train : 70%
+TEST_VAL_SPLIT = 0.5   # Val : 15% , Test : 15%
 
-TARGET = "" # Used for supervised learning tasks
+# Preprocessing pipeline config
 
 BASELINE_MODEL = LogisticRegression()
--- src/sentimental/ingest.py
+++ src/sentimental/ingest.py
@@ -1,30 +1,124 @@
 import os
 
 import pandas as pd
+import mlflow
 from sklearn.model_selection import train_test_split
 
 from .config import config
-from .utils import feature_target_split
+
+
+def drop_missing(df: pd.DataFrame) -> pd.DataFrame:
+    with mlflow.start_run(run_name="Drop missing") as run:
+        mlflow.set_tag("run_id", run.info.run_id)
+
+        df_no_missing = df.dropna(axis=0)
+        drop_count = len(df) - len(df_no_missing)
+        drop_percent = round(100 * drop_count / len(df), 2)
+
+        metrics = {
+            "dataset_size": len(df),
+            "missing_count": drop_count,
+            "missing_percent": drop_percent,
+            "cleaned_size": len(df_no_missing)
+        }
+        mlflow.log_metrics(metrics)
+        mlflow.end_run()
+        return df_no_missing
+
+
+def drop_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+    with mlflow.start_run(run_name="Drop duplicates") as run:
+        mlflow.set_tag("run_id", run.info.run_id)
+
+        df_no_dupes = df.drop_duplicates()
+        drop_count = len(df) - len(df_no_dupes)
+        drop_percent = round(100 * drop_count / len(df), 2)
+
+        metrics = {
+            "dataset_size": len(df),
+            "duplicate_count": drop_count,
+            "duplicate_percent": drop_percent,
+            "cleaned_size": len(df_no_dupes)
+        }
+        mlflow.log_metrics(metrics)
+        mlflow.end_run()
+        return df_no_dupes
+
+
+def drop_conflicts(df: pd.DataFrame) -> pd.DataFrame:
+    data: pd.DataFrame = df.copy()
+    features = list(data.columns.drop(config.TARGET))
+
+    # Step 1 : Find feature groups with >1 unique label
+    grouped = data.groupby(features)[config.TARGET].nunique()
+    conflicting_groups = grouped[grouped > 1]
+
+    # Step 2 : Extract all conflicting rows
+    conflict_keys = conflicting_groups.index
+    df_conflict = pd.DataFrame(list(conflict_keys), columns=features)
+    data.merge(df_conflict, on=features, how="inner")
+
+    # Step 3 : Remove conflicting rows
+    df_no_conflict = data.merge(
+        df_conflict, on=features, how="left", indicator=True
+    )
+    df_no_conflict = df_no_conflict[
+        df_no_conflict["_merge"] == "left_only"
+    ].drop(columns="_merge")
+
+    with mlflow.start_run(run_name="Fix label noise") as run:
+        mlflow.set_tag("run_id", run.info.run_id)
+        noise_count = len(df) - len(df_no_conflict)
+        noise_percent = round(100 * noise_count / len(df), 2)
+        metrics = {
+            "dataset_size": len(df),
+            "noisy_label_count": noise_count,
+            "noisy_label_percent": noise_percent,
+            "cleaned_size": len(df_no_conflict)
+        }
+        mlflow.log_metrics(metrics)
+        mlflow.end_run()
+
+    return df_no_conflict
 
 
 def ingest(raw_path, to_dir):
     rs = config.RANDOM_STATE
+    df = pd.read_csv(raw_path)
+    df = df.drop(labels=["ID"], axis=1)
 
-    x, y = feature_target_split(raw_path)
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=config.TEST_SPLIT, stratify=y, random_state=rs
+    mlflow.set_tracking_uri(config.MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(experiment_name="Data Ingestion")
+    df = drop_missing(df)
+    df = drop_duplicates(df)
+    df = drop_conflicts(df)
+
+    df_train, df_test = train_test_split(
+        df, test_size=config.TRAIN_TEST_SPLIT,
+        stratify=df[config.TARGET], random_state=rs
     )
-    x_train, x_val, y_train, y_val = train_test_split(
-        x_train, y_train, test_size=config.TRAIN_SPLIT*config.VAL_SPLIT,
-        stratify=y_train, random_state=rs)
+    df_val, df_test = train_test_split(
+        df_test, test_size=config.TEST_VAL_SPLIT,
+        stratify=df_test[config.TARGET], random_state=rs
+    )
 
-    df_train = pd.concat([x_train, y_train], axis=1)
-    df_val = pd.concat([x_val, y_val], axis=1)
-    df_test = pd.concat([x_test, y_test], axis=1)
+    df_train.to_csv(to_dir / config.TRAIN_FILE, header=False, index=False)
+    df_val.to_csv(to_dir / config.VALIDATION_FILE, header=False, index=False)
+    df_test.to_csv(to_dir / config.TEST_FILE, header=False, index=False)
 
-    df_train.to_csv(to_dir / config.TRAIN_FILE, header=True, index=False)
-    df_val.to_csv(to_dir / config.VALIDATION_FILE, header=True, index=False)
-    df_test.to_csv(to_dir / config.TEST_FILE, header=True, index=False)
+    with mlflow.start_run(run_name="Split dataset") as run:
+        mlflow.set_tag("run_id", run.info.run_id)
+        metrics = {
+            "dataset_size": len(df),
+            "train_test_split": config.TRAIN_TEST_SPLIT,
+            "test_val_split": config.TEST_VAL_SPLIT,
+            "random_state": rs,
+            "train_size": len(df_train),
+            "val_size": len(df_val),
+            "test_size": len(df_test)
+        }
+        mlflow.log_metrics(metrics)
+        mlflow.end_run()
 
 
 def main():
